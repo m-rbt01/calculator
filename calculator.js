d36fa50219414b82ed47a785f44244e2ef6fb797
Christian Donjuan <cristiandjuan@gmail.com>
2025-09-01 08:55:38 -0500

    feat: add functions that evaluate math operations

---
+++ calculator.js
@@ -0,0 +1,20 @@
+//Global Variables
+let firstOperand;
+let operator;
+let secondOperand;
+
+function add(firstAddend, secondAddend){
+    return firstAddend + secondAddend;
+}
+
+function subtract(minuend, subtrahend){
+    return minuend - subtrahend;
+}
+
+function multiply(multiplicand, multiplier){
+    return multiplicand * multiplier;
+}
+
+function divide(dividend, divisor){
+    return dividend / divisor;
+}
